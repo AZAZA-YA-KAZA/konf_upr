fac82afc4be8fcf8bd91f2caa5e1902a23135212
Aswertti <var.yerkhova@gmail.com>
2024-09-19 00:37:08 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,104 +1,12 @@
-# # import zipfile
-# # import xml.etree.ElementTree as ET
-# #
-# #
-# # def ls(a):
-# #     pass
-# #
-# # def test_ls():
-# #     res = ls(123)
-# #     assert res == a
-# #
-# #
-# # with zipfile.ZipFile('ex1.zip', 'a') as zip:
-# #     root = ET.Element('config')
-# #
-# #     while True:
-# #         comand = input('$ ')
-# #         if comand == 'exit':
-# #             break
-# #         elif comand == 'test':
-# #             print('Da')
-# #         elif comand.startswith('ls '):
-# #             ls(1)
-# #         else:
-# #             print('ti_durakk')
-#
-# import os
-# import zipfile
-# import xml.etree.ElementTree as ET
-#
-#
-# class VirtualShell:
-#     def __init__(self, config_file):
-#         # Чтение конфигурационного файла
-#         self.config_file = config_file
-#         self.zip = zipfile.ZipFile(config_file, 'a')
-#         self.filesystem = {}
-#         # self.current_directory = "/"
-#         # self.load_filesystem(self.vfs_path)
-#
-#     def read_config(self, config_file):
-#         # Открываем файл в режиме чтения
-#         with self.zip_ref.open(config_file, "r") as f:
-#             # Возвращаем содержимое файла
-#             return f.read()
-#
-#
-#     def ls(self):
-#         # Список файлов в текущем каталоге
-#         files = [f for f in self.filesystem if f.startswith(self.current_directory)]
-#         for file in files:
-#             print(os.path.basename(file))
-#
-#     def cd(self, path):
-#         # Изменение текущего каталога
-#         new_path = os.path.join(self.current_directory, path)
-#         if new_path in self.filesystem:
-#             self.current_directory = new_path
-#         else:
-#             print(f"cd: {path}: No such file or directory")
-#
-#     def clear(self):
-#         os.system('cls' if os.name == 'nt' else 'clear')
-#
-#     def du(self):
-#         # Подсчет общего объема файлов в текущем каталоге
-#         total_size = sum(
-#             len(self.filesystem[file]) for file in self.filesystem if file.startswith(self.current_directory))
-#         print(f"Total size: {total_size} bytes")
-#
-#     def run(self):
-#         # Запуск командного интерфейса
-#         while True:
-#             command = input(f"{self.hostname}:{self.current_directory}$ ")
-#             if command == "exit":
-#                 break
-#             elif command == "ls":
-#                 self.ls()
-#             elif command.startswith("cd"):
-#                 _, path = command.split(" ", 1)
-#                 self.cd(path)
-#             elif command == "clear":
-#                 self.clear()
-#             elif command == "du":
-#                 self.du()
-#             else:
-#                 print(f"{command}: command not found")
-#
-#
-# if __name__ == "__main__":
-#     name = input("Enter name of computer ")# получение имени компьютера
-#     shell = VirtualShell('conf.xml', name)#Создание эмулятора
-#     shell.run()#запуск
-
-
 import os
 import zipfile
 import xml.etree.ElementTree as ET
 
+from click import command
+
 
 class VirtualShell:
+
     def __init__(self, config_file):
         # Чтение конфигурационного файла
         self.read_config(config_file)
@@ -163,8 +71,8 @@ class VirtualShell:
 
     def run(self):
         # Запуск командного интерфейса
+        command = input(f"{self.nam}:{self.current_directory}$ ")
         while True:
-            command = input(f"{self.nam}:{self.current_directory}$ ")
             if command == "exit":
                 break
             elif command.startswith("ls"):
@@ -186,6 +94,30 @@ class VirtualShell:
             else:
                 print(f"{command}: command not found")
 
+    def run_test(self, command):
+        # Запуск командного интерфейса
+        print(f"{self.nam}:{self.current_directory}$ "+command)
+        if command == "exit":
+            return
+        elif command.startswith("ls"):
+            if len(command.split()) > 1:
+                _, pu = command.split(" ", 1)
+            else:
+                pu = "ex1.zip"
+            self.ls(pu)
+        elif command.startswith("cd"):
+            if len(command.split()) > 1:
+                _, path = command.split(" ", 1)
+            else:
+                path = "ex1.zip"
+            self.cd(path)
+        elif command == "clear":
+            self.clear()
+        elif command == "du":
+            self.du()
+        else:
+            print(f"{command}: command not found")
+
 
 def write_config(filesystem_path, output_file):
     # Создаем корневой элемент <config>
@@ -194,19 +126,65 @@ def write_config(filesystem_path, output_file):
     nam = input("Enter name of computer ")
     hostname_elem = ET.SubElement(root, 'hostname')
     hostname_elem.text = nam
-
     # Добавляем элемент <filesystem>
     filesystem_elem = ET.SubElement(root, "filesystem")
     filesystem_elem.text = filesystem_path
-
     # Создаем дерево XML и записываем в файл
     tree = ET.ElementTree(root)
     with open(output_file, "wb") as file:
         tree.write(file, encoding='utf-8', xml_declaration=True)
 
+def write_config_test(filesystem_path, output_file, nam):
+    # Создаем корневой элемент <config>
+    root = ET.Element("config")
+    # Добавляем элемент
+    hostname_elem = ET.SubElement(root, 'hostname')
+    hostname_elem.text = nam
+    # Добавляем элемент <filesystem>
+    filesystem_elem = ET.SubElement(root, "filesystem")
+    filesystem_elem.text = filesystem_path
+    # Создаем дерево XML и записываем в файл
+    tree = ET.ElementTree(root)
+    with open(output_file, "wb") as file:
+        tree.write(file, encoding='utf-8', xml_declaration=True)
 
-if __name__ == "__main__":
+def sam():
     script_path = os.path.abspath('ex1.zip')
     write_config(script_path, "config.xml")
     shell = VirtualShell('config.xml')
     shell.run()
+
+def test_1():
+    script_path = os.path.abspath('ex1.zip')
+    write_config_test(script_path, "config.xml", nam = "Aswertti")
+    shell = VirtualShell('config.xml')
+    shell.run_test("ls")
+    shell.run_test("cd")
+    shell.run_test("exit")
+
+
+def test_2():
+    script_path = os.path.abspath('ex1.zip')
+    write_config_test(script_path, "config.xml", nam = "Aswertti")
+    shell = VirtualShell('config.xml')
+    shell.run_test("cd lalala")
+    shell.run_test("clear")
+    shell.run_test("cd")
+    shell.run_test("exit")
+
+
+def test_3():
+    script_path = os.path.abspath('ex1.zip')
+    write_config_test(script_path, "config.xml", nam = "Aswertti")
+    shell = VirtualShell('config.xml')
+    shell.run_test("ls dfrtgyhgb")
+    shell.run_test("cd fgb")
+    shell.run_test("du")
+    shell.run_test("exit")
+
+if __name__ == "__main__":
+    test_1()
+    print()
+    test_2()
+    print()
+    test_3()
